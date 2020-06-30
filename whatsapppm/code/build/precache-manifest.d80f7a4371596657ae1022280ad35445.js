b3fbb48afc10e2d4811c314b98f330988d82ba17
Avinash Thakur <iamavinashthakur.at@gmail.com>
2020-07-01 02:24:44 +0530

    new2

---
+++ whatsapppm/code/build/precache-manifest.d80f7a4371596657ae1022280ad35445.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa1b2c02b5992fdb779fbd5e9e3ab2e7",
+    "url": "/whatsapppm/code/build/index.html"
+  },
+  {
+    "revision": "19693506c791f734d00f",
+    "url": "/whatsapppm/code/build/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "3bb4b5a9844d95124a9d",
+    "url": "/whatsapppm/code/build/static/js/2.4c4be380.chunk.js"
+  },
+  {
+    "revision": "19693506c791f734d00f",
+    "url": "/whatsapppm/code/build/static/js/main.f067a385.chunk.js"
+  },
+  {
+    "revision": "2f89f88e5c3c153de7c7",
+    "url": "/whatsapppm/code/build/static/js/runtime~main.83309143.js"
+  }
+]);
--- whatsapppm/code/build/service-worker.js
+++ whatsapppm/code/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.09efefb618129fcde8ef5609e61ca7b7.js"
+  "/whatsapppm/code/build/precache-manifest.d80f7a4371596657ae1022280ad35445.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/whatsapppm/code/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
